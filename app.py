87ed36970e5b65dd2a680dfd5e5c72ed4370967d
Fernando Mez <fmezzabotta@eventbrite.com>
2018-09-05 13:04:16 -0300

    get performances

--- app.py
+++ app.py
@@ -12,20 +12,13 @@ app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
 
-@app.route("/")
-def hello():
-    return render_template('index.html', name='Mala Fama')
-
-
-@app.route('/artists/', methods=['GET'])
-def get_artists():
-    name = request.args.get('name')
-    response = requests.get('https://www.evbqaapi.com/v3/artists/?token={token}&name_filter={name_filter}'.format(
+def get_artists(name):
+    response = requests.get('https://www.evbqaapi.com/v3/artists/?token={token}&name={name}'.format(
             token=app.config["EB_API_KEY"],
-            name_filter=name,
+            name=name,
         )
     )
-    return response.text
+    return response.content
 
 
 @app.route('/artists/<int:artist_id>/', methods=['GET'])
@@ -40,11 +33,13 @@ def get_artist(artist_id):
 
 @app.route('/performances/', methods=['GET'])
 def get_performances():
-    artist_ids = request.args.get('artist_ids')
+    artist_name = request.args.get('name')
+    artist = json.loads(get_artists(artist_name))
+    artist_id = artist.get('artists')[0].get('id')
     contents = requests.get(
         'http://www.evbqaapi.com/v3/performances?token={token}&artist_ids={artist_ids}&expand={expand}'.format(
             token=app.config['EB_API_KEY'],
-            artist_ids=artist_ids,
+            artist_ids=artist_id,
             expand='event,event.venue',
         ))
     return contents.text
