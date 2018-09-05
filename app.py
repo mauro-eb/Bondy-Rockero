7c0aa9541c8199a4f1d2befbdb5907e4941219e1
Ariel Parra <arielp@eventbrite.com>
2018-09-05 11:13:05 -0300

    Adding api V1

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
 from flask import (
-  Flask,
-  render_template,
+    Flask,
+    jsonify,
+    render_template,
 )
 import json
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
-  return render_template('index.html', name='Elpi')
+    return render_template('index.html', name='Elpi')
+
+
+@app.route('/artists/', methods=['GET'])
+def get_artists():
+    return jsonify([
+        {
+            'artist_id': 1,
+            'name': 'nombre',
+        }
+    ])
+
+
+@app.route('/artists/<int:artist_id>/', methods=['GET'])
+def get_artist(artist_id):
+    return jsonify({
+        'artist_id': artist_id,
+        'name': 'nombre',
+      })
