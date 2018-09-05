9ac368825aaa2dc613bc0c05033f776cdfd30209
Fernando Mez <fmezzabotta@eventbrite.com>
2018-09-05 11:57:02 -0300

    configs

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import (
     render_template,
 )
 import requests
-import os
+import app
 import jsonify
 
 app = Flask(__name__)
@@ -13,20 +13,20 @@ app.config.from_pyfile('config.py')
 @app.route('/artists/', methods=['GET'])
 def get_artists():
     response = requests.get('https://www.evbqaapi.com/v3/artists/?token={token}'.format(
-            token=os.environ["EB_API_KEY"],
+            token=app.config["EB_API_KEY"],
         )
     )
-    return response.content
+    return response.text
 
 
 @app.route('/artists/<int:artist_id>/', methods=['GET'])
 def get_artist(artist_id):
     response = requests.get('https://www.evbqaapi.com/v3/artists/{artist_id}/?token={token}'.format(
             artist_id=artist_id,
-            token=os.environ["EB_API_KEY"],
+            token=app.config["EB_API_KEY"],
         )
     )
-    return response.content
+    return response.text
 
 
 @app.route('/status/', methods=['GET'])
